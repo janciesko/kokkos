1f8c7878d4c724d8e18e4d588e95753e6fd95f85
Nicolas Morales <nmmoral@sandia.gov>
2025-08-13 17:20:43 -0400

    fix clang format
    Signed-off-by: Nicolas Morales <nmmoral@sandia.gov>

--- core/src/View/MDSpan/Kokkos_MDSpan_Header.hpp
+++ core/src/View/MDSpan/Kokkos_MDSpan_Header.hpp
@@ -42,7 +42,6 @@ struct index_pair_like<Kokkos::pair<IdxT1, IdxT2>, IndexType> {
 }  // namespace detail
 }  // namespace Kokkos
 
-
 #define MDSPAN_IMPL_CHECK_PRECONDITION 0
 #include <mdspan/mdspan.hpp>
 
